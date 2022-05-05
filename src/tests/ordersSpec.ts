91cd982f00cfb4c17e98b7cf00b1f45154873901
MedhatAbdelmoniem <87497679+MedhatAbdelmoniem@users.noreply.github.com>
2022-05-05 09:40:31 +0200

    finished the project

--- src/tests/ordersSpec.ts
+++ src/tests/ordersSpec.ts
@@ -1,7 +1,15 @@
 import { OrdersStore } from "../models/orders";
+import supertest from 'supertest';
+import app from '../server';
 
 const store = new OrdersStore;
+const request = supertest(app);
 
 it('should have a currentOrder method', ()=>{
     expect(store.currentOrder).toBeDefined();
+})
+
+it('testing the endpoint with no token', async ()=>{
+    const response = await request.get('/order/1');
+    expect(response.status).toEqual(401);
 })
